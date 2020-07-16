39991f3168055cae0cdd182c152e82ddfb2e9e04
karun <karunahuja1998@gmail.com>
2020-07-16 20:41:21 +0530

    Creating Custom Modules in Node Using NodeJs

---
+++ custom_modules/index.js
@@ -0,0 +1,4 @@
+const mod=require('./mod');
+console.log(mod.avg([3,4]));
+console.log("this is index");
+console.log(mod.name);
---
+++ custom_modules/mod.js
@@ -0,0 +1,14 @@
+console.log("this is mod");
+function average(arr){
+    let sum=0;
+    arr.forEach(element=>{
+        sum+=element;
+    });
+    return sum/arr.length;
+
+}
+module.exports={
+    avg:average,
+    name:"karun",
+    repo:"Github"
+};
